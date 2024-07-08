ad5041b9da31eb928248379e6b225f289bcf0c7c
Eric <ec6862@gmail.com>
2024-07-07 22:03:30 -0400

    added routes and paths

---
+++ creatorverse/src/components/Card.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Card = () => {
+
+}
+
+export default Card;
---
+++ creatorverse/src/pages/AddCreator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddCreator = () => {
+    return (
+        <div>
+        <h1>Add Creator</h1>
+        </div>
+    );
+};
+
+export default AddCreator;
