93fb64cf5a22b574746b9cb0977fc5c4880d44fc
Eric <ec6862@gmail.com>
2024-07-09 04:31:13 -0400

    testing addCreator/card component

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -1,7 +1,19 @@
 import react from "react";
+import { supabase } from "../client";
 
 const Card = () => {
+    // Here, we are fetching the data for the creators
+    // PROGRESS: started async, not finished 7/9
+    const getInfo = async () => {
+        const { data, error } = await supabase.from("creators").select();
+        console.log(data);
+    }
 
+    return (
+        <div>
+            <h1>Card</h1>
+        </div>
+    )
 }
 
 export default Card;
--- creatorverse/src/pages/AddCreator.jsx
+++ creatorverse/src/pages/AddCreator.jsx
@@ -5,16 +5,32 @@ import { supabase } from "../client";
 const AddCreator = () => {
     const addNewCreator = async (event) => {
         event.preventDefault();
-        const { data, error } = await supabase.from("creatorverse");
+        const { data, error } = await supabase.from("creators");
     }
 
 
     return (
         <div>
             <form>
+                <label>Name</label>
                 <input 
                     type="text" 
                     placeholder="Name"
+                />
+                <label>url</label>
+                <input
+                    type="text"
+                    placeholder="url"
+                />
+                <label>description</label>
+                <input 
+                    type="text" 
+                    placeholder="description"
+                />
+                <label>imageURL</label>
+                <input
+                    type="text"
+                    placeholder="imageURL"
                 />
             </form>
         </div>
