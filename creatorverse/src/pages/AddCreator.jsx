ea54a0e0e0865cc9b659df8159a289ac61ec7680
Eric <ec6862@gmail.com>
2024-07-10 02:33:28 -0400

    finished adding creator functions

--- creatorverse/src/pages/AddCreator.jsx
+++ creatorverse/src/pages/AddCreator.jsx
@@ -3,11 +3,16 @@ import { supabase } from "../client";
 
 
 const AddCreator = () => {
+    const [name, setName] = useState("");
+    const [url, setUrl] = useState("");
+    const [description, setDescription] = useState("");
+    const [imageURL, setImageURL] = useState("");
+
     const addNewCreator = async (event) => {
         event.preventDefault();
-        const { data, error } = await supabase.from("creators");
+        const { data, error } = await supabase.from("creators").insert({name: name, url: url, description: description, imageURL: imageURL}).select();
+        console.log(data, error);
     }
-
 
     return (
         <div>
@@ -16,23 +21,28 @@ const AddCreator = () => {
                 <input 
                     type="text" 
                     placeholder="Name"
+                    onChange={(event) => setName(event.target.value)}
                 />
                 <label>url</label>
                 <input
                     type="text"
                     placeholder="url"
+                    onChange={(event) => setUrl(event.target.value)}
                 />
                 <label>description</label>
                 <input 
                     type="text" 
                     placeholder="description"
+                    onChange={(event) => setDescription(event.target.value)}
                 />
                 <label>imageURL</label>
                 <input
                     type="text"
                     placeholder="imageURL"
+                    onChange={(event) => setImageURL(event.target.value)}
                 />
             </form>
+            <button onClick={addNewCreator}>Submit</button>
         </div>
     );
 };
