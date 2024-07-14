8570b3337eb97054264cb1c11e70b6d890b9bd4c
Eric <ec6862@gmail.com>
2024-07-13 23:54:52 -0400

    added basic edit functionalities

--- creatorverse/src/pages/EditCreator.jsx
+++ creatorverse/src/pages/EditCreator.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { supabase } from "../client"
+import { useParams } from "react-router-dom";
 
 const EditCreator = () => {
+    const params = useParams(); // for edit data, we might ahve to use props to get the accurate rather than symbol (7/13)
+    const index = parseInt(params.symbol, 10);
+    const [list, setList] = useState([]);
 
+    useEffect(() => {
+        const getData = async () => {
+            const { data, error } = await supabase
+            .from("creators")
+            .select("*")
+            .eq("id", index);
+            setList(data);
+            if (error)
+                console.error("Error fetching post data: ", error.message);
+        }
+        getData();
+    }, []);
 }
 
 export default EditCreator;
