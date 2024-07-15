0edbfdb35e60ebfa5bcc498bdc12b1088b802bd4
Eric <ec6862@gmail.com>
2024-07-15 00:37:05 -0400

    debugging viewCreator, implementing Card into function

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -1,15 +1,10 @@
 import react from "react";
 import { supabase } from "../client";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 const Card = ({name, url, description, imageURL}) => {
     // Here, we are fetching the data for the creators
     // PROGRESS: started async, not finished 7/9
-    const getInfo = async () => {
-        const { data, error } = await supabase.from("creators").select();
-        console.log(data);
-    }
-
     return (
         <div>
             <h1>Card</h1>
--- creatorverse/src/pages/ViewCreator.jsx
+++ creatorverse/src/pages/ViewCreator.jsx
@@ -7,33 +7,37 @@ const ViewCreator = () => {
     const params = useParams();
     const index = parseInt(params.symbol, 10);
     const [list, setList] = useState([]);
+
     useEffect(()  => {
         const getData = async () => {
             const { data, error } = await supabase
             .from("creators")
             .select("*")
             .eq("id", index);
+
             setList(data);
             if (error)
                 console.error("Error fetching post data: ", error.message);
         }
         getData();
-    }, [])
+    }, [index]);
+
+    useEffect(() => {
+        if (list.length > 0) {
+            console.log("List name: ", list[0].name)
+        }
+    }, [list]) // 7/15 stopped here: Working on why we cannot print list[0].name
+    // Started working on this because we are trying to import Card into this function
+
     return (
         <div>
-            {
-                
-                list.map((post, i) =>
-                    post.name != "" ? (
-                        <>
-                            <p key={i}>Name: {post.name}</p>
-                            <p>URL: {post.url}</p>
-                            <p>Description: {post.description}</p>
-                            <p>Image: {post.imageURL}</p>
-                        </>
-                    ) : <p>No post available</p>
-                )
-            }
+            <>
+                {/* <p key={i}>Name: {post.name}</p>
+                <p>URL: {post.url}</p>
+                <p>Description: {post.description}</p>
+                <p>Image: {post.imageURL}</p> */}
+                {/* <Card value = {list[0].name} key={i}/> */}
+            </>
         </div>
     )
 }
