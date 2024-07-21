863bd4a6931f75d849e94a976040387faf60e9bb
Eric <ec6862@gmail.com>
2024-07-21 16:59:51 -0400

    updated next goals

--- creatorverse/src/App.jsx
+++ creatorverse/src/App.jsx
@@ -7,9 +7,11 @@ import EditCreator from './pages/EditCreator.jsx'
 import ShowCreators from './pages/ShowCreators.jsx'
 import ViewCreator from './pages/ViewCreator.jsx'
 
-// Step 1, 2, 3, 4, 5, 7: Finished
+// Step 1, 2, 3, 4, 5, 7, 8, 9: Finished
 // Step 6: I used Card for viewing a single creator instead of just displaying the stats, might have to change Card's functionality
-// Step 8(Edit/Updating), 9(Deleting): Not Started
+// Step 8a (adding edit button to the component @ Card): Not Started
+// Next: Fixing Card layout on homePage, CSS
+
 
 const App = () => {
   return (
