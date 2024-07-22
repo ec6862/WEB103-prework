52385bf6b1d0a10e1ceaa1b5f5953df4ce6ecec0
Eric <ec6862@gmail.com>
2024-07-22 04:03:08 -0400

    added styling to navbar

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -2,12 +2,12 @@ import react from "react";
 import { supabase } from "../client";
 // import { useParams } from "react-router-dom";
 
-const Card = ({name, url, description, imageURL, order}) => {
+const Card = ({name, url, description, imageURL}) => {
     // Here, we are fetching the data for the creators
     // PROGRESS: started async, not finished 7/9
     return (
         <div>
-            <p>{order} Name: {name}</p>
+            <p>Name: {name}</p>
         </div>
     )
 }
