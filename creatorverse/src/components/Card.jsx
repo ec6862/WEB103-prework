25ca5b7257b334936e653cf2a4f7fb907a0d3c21
Eric <ec6862@gmail.com>
2024-07-23 03:11:35 -0400

    added URL link functionalties

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -5,10 +5,16 @@ import { supabase } from "../client";
 const Card = ({name, url, description, imageURL}) => {
     // Here, we are fetching the data for the creators
     // PROGRESS: started async, not finished 7/9
+    const ensureHttp = (url) => {
+        if (!/^https?:\/\//i.test(url)) // allows the url to work by inputting "https://"" in front of it if it DOESN'T have one
+            return 'https://' + url;
+        return url;
+    }
+
     return (
         <div>
             <p>Name: {name}</p>
-            <p>URL: {url}</p>
+            <a href={ensureHttp(url)} target="_blank" rel="noopener noreferrer">URL: {url}</a> {/*for some reason, we need "https://" for the link to work */}
             <p>Description: {description}</p>
             <p>Image: {imageURL}</p>
         </div>
